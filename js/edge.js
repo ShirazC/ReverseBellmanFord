8fa086d1b23fba44eec2efe0ce75372ba221ae74
Yanni <kamyanni@gmail.com>
2019-08-14 11:46:32 -0400

    Adding Vertex, Edge, and Heap

---
+++ js/edge.js
@@ -0,0 +1,8 @@
+class Edge {
+
+  function Edge(e, v, distance) {
+    this.from = e;
+    this.to = v;
+    this.distance = distance;
+  }
+}
--- js/graphNode.js
+++ js/graphNode.js
@@ -1,33 +1,38 @@
 class GraphNode {
 
-  constructor() {
-    nodeList = [];
+  // constructor() {
+  //   nodeList = [];
+  // }
+  //
+  // function add(location) {
+  //   this.location = location;
+  //   // Will be empty for all passengers
+  //
+  //   if (!addressExists(location)) {
+  //     nodeList.push(location);
+  //   }
+  //
+  //   return this;
+  // }
+  //
+  // function addressExists(address) {
+  //   var bool = false;
+  //
+  //   for (var i = 0; i < nodeList.length && !bool; i++) {
+  //     if (nodeList[i].location == address) {
+  //       bool = true;
+  //     }
+  //   }
+  //
+  //   return bool;
+  // }
+  //
+  // function getAddresses() {
+  //   return nodeList;
+  // }
+
+  function Vertex(id) {
+    this.id = id;
   }
-
-  function add(location) {
-    this.location = location;
-    // Will be empty for all passengers
-
-    if (!addressExists(location)) {
-      nodeList.push(location);
-    }
     
-    return this;
-  }
-
-  function addressExists(address) {
-    var bool = false;
-
-    for (var i = 0; i < nodeList.length && !bool; i++) {
-      if (nodeList[i].location == address) {
-        bool = true;
-      }
-    }
-
-    return bool;
-  }
-
-  function getAddresses() {
-    return nodeList;
-  }
 }
